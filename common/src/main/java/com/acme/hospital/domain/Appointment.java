27d47ed7dec7d4cb47d520506acf3679c2ace426
Bihari Tamás <biharitomi@gmail.com>
2012-10-25 11:11:17 +0200

    adding Appointment to domain

--- common/src/main/java/com/acme/hospital/domain/Appointment.java
+++ common/src/main/java/com/acme/hospital/domain/Appointment.java
@@ -1,5 +1,106 @@
 package com.acme.hospital.domain;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="appointment")
 public class Appointment {
 	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column(nullable=false)
+	private Date date;
+	
+	@ManyToOne
+	private Doctor doctor;
+	
+	@ManyToOne
+	private Patient patient;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Doctor getDoctor() {
+		return doctor;
+	}
+
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	@Override
+	public String toString() {
+		return "Appointment [id=" + id + ", date=" + date + ", doctor="
+				+ doctor + ", patient=" + patient + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((doctor == null) ? 0 : doctor.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((patient == null) ? 0 : patient.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Appointment other = (Appointment) obj;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (doctor == null) {
+			if (other.doctor != null)
+				return false;
+		} else if (!doctor.equals(other.doctor))
+			return false;
+		if (id != other.id)
+			return false;
+		if (patient == null) {
+			if (other.patient != null)
+				return false;
+		} else if (!patient.equals(other.patient))
+			return false;
+		return true;
+	}
 }
