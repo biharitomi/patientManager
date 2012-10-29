e73f7024508cd273b84d7b06d65df82d027971a1
Adam Szabo <adam89taylor@gmail.com>
2012-10-29 13:46:33 +0100

    login by spring security fix

--- gui/src/main/java/com/acme/hospital/web/managedbeans/LoginManagedBean.java
+++ gui/src/main/java/com/acme/hospital/web/managedbeans/LoginManagedBean.java
@@ -1,23 +1,43 @@
 package com.acme.hospital.web.managedbeans;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
 
-@ManagedBean(name="LoginManagedBean")
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@ManagedBean(name = "LoginManagedBean")
+@SessionScoped
 public class LoginManagedBean {
-	private String userName="alma";
-	private String password="";
-	
-	public String login(){
-		
-		System.out.println("OK Button clicked!");
-		return "/views/index?faces-redirect=true";
+	private String userName = "";
+	private String password = "";
+
+	@ManagedProperty(value = "#{authenticationManager}")
+	private AuthenticationManager am;
+
+	public String login() {
+		try {
+			Authentication request = new UsernamePasswordAuthenticationToken(
+					this.getUserName(), this.getPassword());
+			Authentication result = am.authenticate(request);
+			SecurityContextHolder.getContext().setAuthentication(result);
+		} catch (BadCredentialsException e) {
+			return "login";
+		}
+		return "views/index";
 	}
 
 	public String getUserName() {
+		System.out.println("get username");
 		return userName;
 	}
 
 	public void setUserName(String userName) {
+		System.out.println("set username = " + userName);
 		this.userName = userName;
 	}
 
@@ -26,6 +46,15 @@ public class LoginManagedBean {
 	}
 
 	public void setPassword(String password) {
+		System.out.println("set password = " + password);
 		this.password = password;
+	}
+
+	public AuthenticationManager getAm() {
+		return am;
+	}
+
+	public void setAm(AuthenticationManager am) {
+		this.am = am;
 	}
 }
