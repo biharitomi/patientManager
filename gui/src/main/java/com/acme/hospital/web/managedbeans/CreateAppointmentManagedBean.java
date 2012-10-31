8111a8e4135089253c555fb0b2a5e1c07445e55e
Adam Szabo <adam89taylor@gmail.com>
2012-10-31 11:23:15 +0100

    Add message about the appointment creation

--- gui/src/main/java/com/acme/hospital/web/managedbeans/CreateAppointmentManagedBean.java
+++ gui/src/main/java/com/acme/hospital/web/managedbeans/CreateAppointmentManagedBean.java
@@ -3,11 +3,14 @@ package com.acme.hospital.web.managedbeans;
 import java.util.Date;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
+import org.primefaces.context.RequestContext;
 import org.springframework.security.authentication.AuthenticationManager;
 
 import com.acme.hospital.domain.Doctor;
@@ -17,26 +20,39 @@ import com.acme.hospital.web.adapter.AppointmentFacadeSpringAdapter;
 @ManagedBean(name = "createAppointmentMB")
 @RequestScoped
 public class CreateAppointmentManagedBean {
-	
+
 	@ManagedProperty(value = "#{appointmentFacadeSpringAdapter}")
 	private AppointmentFacadeSpringAdapter afsa;
-	
+
 	@ManagedProperty(value = "#{LoginManagedBean}")
 	private LoginManagedBean loginManagedBean;
-	
+
 	private Date date;
-	
+
 	private Patient selectedPatient;
-	
+
 	private List<Patient> allPatients;
-	
-	public boolean createAppointment() {
-		if(date == null || selectedPatient == null) {
-			return false;
+
+	public void createAppointment() {
+		boolean result = false;
+		if (date == null || selectedPatient == null) {
 		} else {
 			String loggedInDoctorName = loginManagedBean.getLoggedInUser();
-			Doctor d = afsa.getAppointmentFacade().getDoctorByName(loggedInDoctorName); 
-			return afsa.getAppointmentFacade().createAppointment(d, selectedPatient, date);
+			Doctor d = afsa.getAppointmentFacade().getDoctorByName(
+					loggedInDoctorName);
+			result = afsa.getAppointmentFacade().createAppointment(d,
+					selectedPatient, date);
+		}
+		if (result) {
+			FacesContext.getCurrentInstance().addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Info",
+							"The appointment creation was successful"));
+		} else {
+			FacesContext.getCurrentInstance().addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error",
+							"The appointment creation was unsuccessful"));
 		}
 	}
 
@@ -47,9 +63,9 @@ public class CreateAppointmentManagedBean {
 	public void setDate(Date date) {
 		this.date = date;
 	}
-	
-	public List<Patient> getAllPatients(){
-		this.allPatients=afsa.getAllPatients();
+
+	public List<Patient> getAllPatients() {
+		this.allPatients = afsa.getAllPatients();
 		return this.allPatients;
 	}
 
